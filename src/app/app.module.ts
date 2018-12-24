7445b072c21faf0ecb8838279c8757244dd0a8f5
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2018-12-24 13:46:11 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { PagesComponent } from './pages/pages.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { PageNotFountComponent } from './shared/page-not-fount/page-not-fount.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    Graficas1Component,
+    PagesComponent,
+    ProgressComponent,
+    BreadcrumbsComponent,
+    HeaderComponent,
+    SidebarComponent,
+    PageNotFountComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +36,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/shared/page-not-fount/page-not-fount.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-fount',
+  templateUrl: './page-not-fount.component.html',
+  styles: []
+})
+
+export class PageNotFountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
